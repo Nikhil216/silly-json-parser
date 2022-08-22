94e92133f4d059792bbb95f3721d0bcbfed77eb2
Nikhil216 <nmdevadiga216@gmail.com>
2022-08-22 22:37:19 +0530

    test: added test for
      - notChar
      - peek
      - or
      - peekNot
      - zeroOrMoreChar

--- test/Spec.hs
+++ test/Spec.hs
@@ -98,10 +98,146 @@ testList =
           (S.matchList . BLU.fromString $ "[1]"))
     ]
 
+testNotChar =
+  TestList
+    [ TestCase
+        (assertEqual
+          "Should escape on end stream"
+          (S.MatchEnd "")
+          ((S.matchNotChar '"') . BLU.fromString $ ""))
+    , TestCase
+        (assertEqual
+          "Should error on double quote"
+          (S.MatchErr (BLU.fromString "\"") "Not expected but found it")
+          ((S.matchNotChar '"') . BLU.fromString $ "\""))
+    , TestCase
+        (assertEqual
+          "Should match on space"
+          (S.MatchOk BL.empty " ")
+          ((S.matchNotChar '"') . BLU.fromString $ " "))
+    ]
+
+testPeek =
+  TestList
+    [ TestCase
+        (assertEqual
+          "Should escape on end stream"
+          (S.MatchEnd "")
+          ((S.peek $ S.matchChar ' ') . BLU.fromString $ ""))
+    , TestCase
+        (assertEqual
+          "Should error on peeking 'a'"
+          (S.MatchErr (BLU.fromString "a ") "Found a instead of  ")
+          ((S.peek $ S.matchChar ' ') . BLU.fromString $ "a "))
+    , TestCase
+        (assertEqual
+          "Should match on single ' ' without trail"
+          (S.MatchOk (BLU.fromString " ") "")
+          ((S.peek $ S.matchChar ' ') . BLU.fromString $ " "))
+    , TestCase
+        (assertEqual
+          "Should match on single ' ' with trail"
+          (S.MatchOk (BLU.fromString " a") "")
+          ((S.peek $ S.matchChar ' ') . BLU.fromString $ " a"))
+    , TestCase
+        (assertEqual
+          "Should match on single '  ' with ' ' as trail"
+          (S.MatchOk (BLU.fromString "  ") "")
+          ((S.peek $ S.matchChar ' ') . BLU.fromString $ "  "))
+    ]
+
+testPeekNot =
+  TestList
+    [ TestCase
+        (assertEqual
+          "Should escape on end stream"
+          (S.MatchEnd "")
+          ((S.peek $ S.matchNotChar ' ') . BLU.fromString $ ""))
+    , TestCase
+        (assertEqual
+          "Should match on peeking 'a'"
+          (S.MatchOk (BLU.fromString "a ") "")
+          ((S.peek $ S.matchNotChar ' ') . BLU.fromString $ "a "))
+    , TestCase
+        (assertEqual
+          "Should error on single ' ' without trail"
+          (S.MatchErr (BLU.fromString " ") "Not expected but found it")
+          ((S.peek $ S.matchNotChar ' ') . BLU.fromString $ " "))
+    , TestCase
+        (assertEqual
+          "Should error on double ' ' without trail"
+          (S.MatchErr (BLU.fromString "  ") "Not expected but found it")
+          ((S.peek $ S.matchNotChar ' ') . BLU.fromString $ "  "))
+    , TestCase
+        (assertEqual
+          "Should match on double 'b' without trail"
+          (S.MatchOk (BLU.fromString "bb") "")
+          ((S.peek $ S.matchNotChar ' ') . BLU.fromString $ "bb"))
+    ]
+
+testOr =
+  TestList
+    [ TestCase
+        (assertEqual
+          "Should escape on end stream"
+          (S.MatchEnd "")
+          (((S.matchChar 'a') `S.matchOr` (S.matchChar 'b')) . BLU.fromString $ ""))
+    , TestCase
+        (assertEqual
+          "Should match on 'a'"
+          (S.MatchOk (BLU.fromString "bc") "a")
+          (((S.matchChar 'a') `S.matchOr` (S.matchChar 'b')) . BLU.fromString $ "abc"))
+    , TestCase
+        (assertEqual
+          "Should match on 'b'"
+          (S.MatchOk (BLU.fromString "ba") "b")
+          (((S.matchChar 'a') `S.matchOr` (S.matchChar 'b')) . BLU.fromString $ "bba"))
+    , TestCase
+        (assertEqual
+          "Should error on 'c'"
+          (S.MatchErr (BLU.fromString "ccc") "Found c instead of b")
+          (((S.matchChar 'a') `S.matchOr` (S.matchChar 'b')) . BLU.fromString $ "ccc"))
+    ]
+
+testZeroOrMoreChar =
+  TestList
+    [ TestCase
+        (assertEqual
+          "Should escape on end stream"
+          (S.MatchEnd "")
+          ((S.matchZeroOrMoreChar $ S.matchChar '"') . BLU.fromString $ ""))
+    , TestCase
+        (assertEqual
+          "Should match on single 'a' with trail"
+          (S.MatchOk (BLU.fromString " ") "a")
+          ((S.matchZeroOrMoreChar $ S.matchChar '"') . BLU.fromString $ "a "))
+    , TestCase
+        (assertEqual
+          "Should match on single 'a' without trail"
+          (S.MatchOk BL.empty "a")
+          ((S.matchZeroOrMoreChar $ S.matchChar '"') . BLU.fromString $ "a"))
+    , TestCase
+        (assertEqual
+          "Should match on triple 'a' with trail"
+          (S.MatchOk (BLU.fromString " ") "aaa")
+          ((S.matchZeroOrMoreChar $ S.matchChar '"') . BLU.fromString $ "aaa "))
+    , TestCase
+        (assertEqual
+          "Should match on triple 'a' without trail"
+          (S.MatchOk BL.empty "aaa")
+          ((S.matchZeroOrMoreChar $ S.matchChar '"') . BLU.fromString $ "aaa"))
+    ]
+
 testSuite =
   TestList
-    [ testInteger
+    [ testNotChar
+    , testPeek
+    , testOr
+    , testPeekNot
+    , testZeroOrMoreChar
+    , testInteger
     , testString
+    , testList
     ]
 
 main :: IO Counts
